b52c3c858603ceba66612f8c858eee9abccafe93
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-06-01 10:27:36 +0300

    GridTest
    GridTest

---
+++ Tower Defence/Assets/GridTileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTileScript : MonoBehaviour
+{
+
+    public Material red;
+    public Material green;
+    public Material brown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = brown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() {
+        GetComponent<Renderer>().material = green;
+    }
+
+    void OnMouseExit() {
+        GetComponent<Renderer>().material = brown;
+    }
+}
