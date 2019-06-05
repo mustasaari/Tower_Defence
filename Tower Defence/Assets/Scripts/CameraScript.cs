5a276173e1c558db5e72fb286f6c6553759ff65c
Mikko Mustasaari <mikko.mustasaari@tuni.fi>
2019-06-05 13:19:21 +0300

    GameManager
    GameManager

--- Tower Defence/Assets/Scripts/CameraScript.cs
+++ Tower Defence/Assets/Scripts/CameraScript.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
-    public GameObject target;//the target object
     private float speedMod = 10.0f;//a speed modifier
     private Vector3 point;//the coord to the point where the camera looks at
    
     void Start ()
     {
-    point = target.transform.position;//get target's coords
+    point = new Vector3(0,0,-10);//get target's coords
     transform.LookAt(point);//makes the camera look to it
     }
    
