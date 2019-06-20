755b1d2bf3d52ee7e51f598c437afbfd7f09ff80
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-06-20 11:07:39 +0300

    AoE particle
    AoE particle

---
+++ Tower Defence/Assets/ParticleDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroyer : MonoBehaviour
+{
+
+    ParticleSystem pasy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pasy = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pasy.isPlaying) {
+            Destroy(gameObject);
+        }
+    }
+}
