813d071e0c1909e945de1d034529667424e566ab
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-07-05 21:11:02 +0300

    Skill Tooltips & Icon updates
    Skill Tooltips & Icon updates

---
+++ Tower Defence/Assets/SkillTooltipScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTooltipScript : MonoBehaviour
+{
+    [TextArea(3,10)]
+    public string tooltiptext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void showTooltip(string tex) {
+
+    }
+
+    public void OnMouseEnter() {
+        //GameObject.FindGameObjectWithTag("SkillTooltip").SetActive(true);
+        GameObject go = GameObject.FindGameObjectWithTag("SkillTooltip");
+        go.GetComponent<Renderer>().enabled = true;
+        go.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = tooltiptext;
+    }
+
+    public void OnMouseExit() {
+            //GameObject.FindGameObjectWithTag("SkillTooltip").SetActive(false);
+            GameObject go = GameObject.FindGameObjectWithTag("SkillTooltip");
+            go.GetComponent<Renderer>().enabled = false;
+            go.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = "";
+        }
+
+
+}
