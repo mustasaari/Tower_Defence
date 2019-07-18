6e5aea2b19817b4b234c452d0a1790f7d9fef52d
Saku Tynjälä <sakutynjala@gmail.com>
2019-07-18 13:26:20 +0300

    SlidingNumbers script

---
+++ Tower Defence/Assets/Scripts/SlidingNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlidingNumbers : MonoBehaviour
+{
+    public Text expCounter;
+    public float animTime = 1.5f;
+
+    private float desiredNumber;
+    private float initialNumber;
+    private float currentNumber;
+    private bool startCount;
+
+    public void setNumber(float v){
+        initialNumber = currentNumber;
+        desiredNumber = v;
+    }
+
+    public void addToNumber(float v){
+        initialNumber = currentNumber;
+        desiredNumber += v;
+    }
+
+    void Update(){
+        if(currentNumber != desiredNumber && startCount){
+            if(initialNumber < desiredNumber){
+                currentNumber += (animTime * Time.deltaTime) * (desiredNumber - initialNumber);
+                if(currentNumber >= desiredNumber){
+                    currentNumber = desiredNumber;
+                }
+            }
+            else{
+                currentNumber -= (animTime * Time.deltaTime) * (initialNumber - desiredNumber);
+                if(currentNumber <= desiredNumber){
+                    currentNumber = desiredNumber;
+                }
+            }
+        }
+    }
+    public static void setCountBool(){
+
+    }
+}
