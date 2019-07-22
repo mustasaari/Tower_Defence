e22913b544b2aab67a0885483c4d5454b33ed871
Saku Tynjälä <sakutynjala@gmail.com>
2019-07-22 14:11:46 +0300

    sound for exp counter.

--- Tower Defence/Assets/Scripts/SlidingNumbers.cs
+++ Tower Defence/Assets/Scripts/SlidingNumbers.cs
@@ -11,6 +11,37 @@ public class SlidingNumbers : MonoBehaviour
     private float initialNumber;
     private float currentNumber;
     private bool startCount;
+    private bool audioplaying;
+    public AudioSource aud;
+
+    void Start(){
+        audioplaying = false;
+    }
+
+    void Update(){
+        if(currentNumber != desiredNumber && startCount){
+            startPlayingMusic();
+            if(initialNumber < desiredNumber){
+                currentNumber += (animTime * Time.deltaTime) * (desiredNumber - initialNumber);
+                if(currentNumber >= desiredNumber){
+                    currentNumber = desiredNumber;
+                    audioplaying = true;
+                    stopPlayingMusic();
+                    setCountBool(false);
+                }
+            }
+            else{
+                currentNumber -= (animTime * Time.deltaTime) * (initialNumber - desiredNumber);
+                if(currentNumber <= desiredNumber){
+                    currentNumber = desiredNumber;
+                    audioplaying = true;
+                    stopPlayingMusic();
+                    setCountBool(false);
+                }
+            }
+            expCounter.text = currentNumber.ToString("0");
+        }
+    }
 
     public void setNumber(float v){
         initialNumber = currentNumber;
@@ -27,29 +58,20 @@ public class SlidingNumbers : MonoBehaviour
         desiredNumber = end;
     }
 
-    void Update(){
-        if(currentNumber != desiredNumber && startCount){
-            if(initialNumber < desiredNumber){
-                currentNumber += (animTime * Time.deltaTime) * (desiredNumber - initialNumber);
-                if(currentNumber >= desiredNumber){
-                    currentNumber = desiredNumber;
-                    setCountBool(false);
-                }
-            }
-            else{
-                currentNumber -= (animTime * Time.deltaTime) * (initialNumber - desiredNumber);
-                if(currentNumber <= desiredNumber){
-                    currentNumber = desiredNumber;
-                    setCountBool(false);
-                }
-            }
-            expCounter.text = currentNumber.ToString("0");
-        }
-    }
     public void setCountBool(bool a){
         startCount = a;
     }
+
     public bool getCountBool(){
         return startCount;
+    }
+
+    public void startPlayingMusic(){
+        aud.volume = PlayerPrefs.GetFloat("SFXvolume", 0.5f);
+        aud.Play();
+    }
+
+    public void stopPlayingMusic(){
+        aud.Stop();
     }
 }
