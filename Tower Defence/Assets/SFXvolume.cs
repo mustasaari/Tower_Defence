21002ed5b84bf0c32f05f2a9c339fd62177ce856
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-07-23 16:32:25 +0300

    Crystal explosion volume fix
    Crystal explosion volume fix

---
+++ Tower Defence/Assets/SFXvolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXvolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SFXvolume", 0.5f);
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
