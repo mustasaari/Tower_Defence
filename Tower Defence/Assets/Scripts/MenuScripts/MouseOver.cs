0e3fea506cf6783d84db98afd6fef2f5dc4f6a6c
Saku Tynjälä <sakutynjala@gmail.com>
2019-07-25 14:00:23 +0300

    Buttons Highlight & MouseOver Sound

--- Tower Defence/Assets/Scripts/MenuScripts/MouseOver.cs
+++ Tower Defence/Assets/Scripts/MenuScripts/MouseOver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
  
 public class MouseOver: MonoBehaviour, IPointerEnterHandler{
 
@@ -10,6 +11,15 @@ public class MouseOver: MonoBehaviour, IPointerEnterHandler{
     
     public void OnPointerEnter(PointerEventData eventData)
     {
-        AudioSource.PlayClipAtPoint(mouseOverAudio, Camera.main.transform.position, PlayerPrefs.GetFloat("SFXvolume", 0.5f));
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if(sceneName.Equals("Scene1")){
+            transform.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SFXvolume", 0.5f);
+            transform.GetComponent<AudioSource>().Play();
+        }
+        else if(transform.GetComponent<Button>().interactable && !sceneName.Equals("Scene1")){
+            AudioSource.PlayClipAtPoint(mouseOverAudio, Camera.main.transform.position, PlayerPrefs.GetFloat("SFXvolume", 0.5f));
+        }
     }
 }
