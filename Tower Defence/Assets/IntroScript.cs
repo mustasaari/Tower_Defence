0572c073a2cc12dda2293d71d9661cd0522f6dc1
mustasaari <36700246+mustasaari@users.noreply.github.com>
2019-07-30 23:42:18 +0300

    Intro text and music
    Intro text and music

--- Tower Defence/Assets/IntroScript.cs
+++ Tower Defence/Assets/IntroScript.cs
@@ -6,9 +6,20 @@ using UnityEngine.SceneManagement;
 public class IntroScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    float volumeLevel;
+    bool fadeToNextLevel;
+    float sceneStartTime;
+
     void Start()
     {
-        
+        volumeLevel = PlayerPrefs.GetFloat("Musicvolume", 0.5f);
+        GetComponent<AudioSource>().volume = volumeLevel;
+        GetComponent<AudioSource>().Play();
+
+        sceneStartTime = Time.time;
+
+        fadeToNextLevel = false;
     }
 
     // Update is called once per frame
@@ -17,6 +28,12 @@ public class IntroScript : MonoBehaviour
         if (Input.anyKey) {
             Debug.Log("A key or mouse click has been detected");
             GameObject.Find("LevelChanger").GetComponent<LevelChanger>().FadeToLevel(1);
+            fadeToNextLevel = true;
+        }
+
+        if (fadeToNextLevel) {
+            volumeLevel -= 0.3f * Time.deltaTime;
+            GetComponent<AudioSource>().volume = volumeLevel;
         }
     }
 }
